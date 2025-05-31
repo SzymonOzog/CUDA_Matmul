6e8faa15973a5f4d72e140283fb231fc96857210
SzymonOzog <szymon.ozog@gmail.com>
2025-05-31 14:22:52 +0200

    Simplify index calculation

--- ptx_helpers.cuh
+++ ptx_helpers.cuh
@@ -23,8 +23,8 @@ static __device__ __forceinline__ void load_tile_a(mma_tile<16, 16>& a_tile, con
 static __device__ __forceinline__ void load_tile_a_shared(mma_tile<16, 16>& a_tile, const half* mat, const int stride, const int lane_id)
 {
     uint32_t* A = reinterpret_cast<uint32_t*>(a_tile.x);
-    int row = ((lane_id/8)%2) * 8 + lane_id%8;
-    int col = ((lane_id/16))*8;
+    int row = lane_id%16;
+    int col = (lane_id/16)*8;
     const half* addr = mat + row * stride + col;// + lane_id%8;
     asm volatile("ldmatrix.sync.aligned.m8n8.x4.b16  {%0, %1, %2, %3}, [%4];"
             : "=r"(A[0]), "=r"(A[1]), "=r"(A[2]), "=r"(A[3]) : "l"(addr));
@@ -49,8 +49,8 @@ template<int S_BITS>
 static __device__ __forceinline__ void load_tile_a_shared_swizzle(mma_tile<16, 16>& a_tile, const half* mat, const int off_base, const int stride, const int lane_id)
 {
     uint32_t* A = reinterpret_cast<uint32_t*>(a_tile.x);
-    int row = ((lane_id/8)%2) * 8 + lane_id%8;
-    int col = ((lane_id/16))*8;
+    int row = lane_id%16;
+    int col = (lane_id/16)*8;
     int off = off_base + row * stride + col;
     off = off^((off&(S_MASK<<S_BITS))>>S_BITS);
     const half* addr = mat + off;// + lane_id%8;
@@ -87,8 +87,8 @@ static __device__ __forceinline__ void load_tile_b(mma_tile<16, 16>& b_tile, con
 static __device__ __forceinline__ void load_tile_b_shared(mma_tile<16, 16>& b_tile, const half* mat, const int stride, const int lane_id)
 {
     uint32_t* A = reinterpret_cast<uint32_t*>(b_tile.x);
-    int row = ((lane_id/8)%2) * 8 + lane_id%8;
-    int col = ((lane_id/16))*8;
+    int row = lane_id%16;
+    int col = (lane_id/16)*8;
     const half* addr = mat + row * stride + col;// + lane_id%8;
     asm volatile("ldmatrix.sync.aligned.m8n8.x4.trans.b16  {%0, %1, %2, %3}, [%4];"
             : "=r"(A[0]), "=r"(A[1]), "=r"(A[2]), "=r"(A[3]) : "l"(addr));
@@ -98,8 +98,8 @@ template<int S_BITS>
 static __device__ __forceinline__ void load_tile_b_shared_swizzle(mma_tile<16, 16>& b_tile, const half* mat, const int off_base, const int stride, const int lane_id)
 {
     uint32_t* A = reinterpret_cast<uint32_t*>(b_tile.x);
-    int row = ((lane_id/8)%2) * 8 + lane_id%8;
-    int col = ((lane_id/16))*8;
+    int row = lane_id%16;
+    int col = (lane_id/16)*8;
     int off = off_base + row * stride + col;
     off = off^((off&(S_MASK<<S_BITS))>>S_BITS);
     const half* addr = mat + off;
