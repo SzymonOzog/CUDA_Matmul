229683f29758956a5443015479db48da4d27e179
SzymonOzog <szymon.ozog@gmail.com>
2025-06-19 07:20:31 +0200

    Revert "WIP"
    This reverts commit 9f30a7e8dc6398094b883da3cddc3e8f2c8502ed.

--- tensor_cores_async_big_tiles_double_buffering_fb.cu
+++ tensor_cores_async_big_tiles_double_buffering_fb.cu
@@ -126,9 +126,6 @@ __global__ void tensor_core_matmul_async_swizzle_BT_DB_FB(int n_elem, const half
         stage = (stage+1)%2;
     }
 
-    CP_ASYNC_WAIT_GROUP(0);
-    __syncthreads();
-
     for(int32_t i = 0; i<OUT_TILES; i++)
     {
         int32_t output_row = matrix_a_row + i*WMMA_MKN;
@@ -137,19 +134,19 @@ __global__ void tensor_core_matmul_async_swizzle_BT_DB_FB(int n_elem, const half
             int32_t output_col = matrix_b_col + j*WMMA_MKN;
             if (output_row < n_elem && output_col < n_elem)
             {
-                // for (int k = 0; k<4; k++)
-                // {
-                //     reinterpret_cast<half2*>(&c[(output_row + (lane_id>>2) + (k%2)*8)*n_elem + output_col + (k/2)*8])[lane_id%4]
-                //         = acc[i][j].x[k];
-                // }
-                int4 st;
-                int row = lane_id%16;
-                int col = (lane_id/16)*8;
-                st.x = get_at(acc[i][j], (lane_id%8)*4 +  0, lane_id/8);
-                st.y = get_at(acc[i][j], (lane_id%8)*4 +  1, lane_id/8);
-                st.z = get_at(acc[i][j], (lane_id%8)*4 +  2, lane_id/8);
-                st.w = get_at(acc[i][j], (lane_id%8)*4 +  3, lane_id/8);
-                reinterpret_cast<int4*>(&c[(output_row + row)*n_elem + output_col + col])[0] = st;
+                for (int k = 0; k<4; k++)
+                {
+                    reinterpret_cast<half2*>(&c[(output_row + (lane_id>>2) + (k%2)*8)*n_elem + output_col + (k/2)*8])[lane_id%4]
+                        = acc[i][j].x[k];
+                }
+                // int4 st;
+                // int row = lane_id%16;
+                // int col = (lane_id/16)*8;
+                // st.x = get_at(acc[i][j], (lane_id%8)*4 +  0, lane_id/8);
+                // st.y = get_at(acc[i][j], (lane_id%8)*4 +  1, lane_id/8);
+                // st.z = get_at(acc[i][j], (lane_id%8)*4 +  2, lane_id/8);
+                // st.w = get_at(acc[i][j], (lane_id%8)*4 +  3, lane_id/8);
+                // reinterpret_cast<int4*>(&c[(output_row + row)*n_elem + output_col + col])[0] = st;
             }
         }
     }
@@ -158,7 +155,6 @@ __global__ void tensor_core_matmul_async_swizzle_BT_DB_FB(int n_elem, const half
     const int32_t matrix_b_col2 = matrix_b_col + gridDim.y * BN*WMMA_MKN;
     if(matrix_b_col2 >= n_elem)
         return;
-
     stage = (stage+1)%2;
     for(int i = 0; i < OUT_TILES; i++)
     {
@@ -172,47 +168,25 @@ __global__ void tensor_core_matmul_async_swizzle_BT_DB_FB(int n_elem, const half
         }
     }
 
-    const half* b_curr2 = b + (n_elem - 2*BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN + gridDim.y * BN*WMMA_MKN;
-
-    for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
-            i < BN*BK*WMMA_MKN*WMMA_MKN;
-            i+=blockDim.x*blockDim.y*8)
-    {
-        uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
-        const half* b_gmem_curr = &b_curr2[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
-        CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
-    }
-    CP_ASYNC_COMMIT_GROUP();
-    unsigned int b_stage = (stage + 1)%2;
-    for (int k = 0; k<BK; k+=2)
-    {
-        for (int n = 0; n < OUT_TILES; n++)
-        {
-            load_tile_a_shared_swizzle<S_BITS_A>(a_tile[0][n], a_smem + stage*A_ST_STRIDE, (laneM*OUT_TILES + n)*BK*WMMA_MKN*WMMA_MKN + k*WMMA_MKN, BK*WMMA_MKN, lane_id);
-            load_tile_a_shared_swizzle<S_BITS_A>(a_tile[1][n], a_smem + stage*A_ST_STRIDE, (laneM*OUT_TILES + n)*BK*WMMA_MKN*WMMA_MKN + (k+1)*WMMA_MKN, BK*WMMA_MKN, lane_id);
-        }
-        for (int n = 0; n < OUT_TILES; n++)
-        {
-            load_tile_b_shared_swizzle<S_BITS_B>(b_tile[0], b_smem + b_stage*B_ST_STRIDE, (k*BN*WMMA_MKN + laneN*OUT_TILES + n)*WMMA_MKN, BN*WMMA_MKN, lane_id);
-            load_tile_b_shared_swizzle<S_BITS_B>(b_tile[1], b_smem + b_stage*B_ST_STRIDE, ((k+1)*BN*WMMA_MKN + laneN*OUT_TILES + n)*WMMA_MKN, BN*WMMA_MKN, lane_id);
-            for (int m = 0; m < OUT_TILES; m++)
-            {
-                mma(a_tile[0][m], b_tile[0], acc[m][n]);
-            }
-            for (int m = 0; m < OUT_TILES; m++)
-            {
-                mma(a_tile[1][m], b_tile[1], acc[m][n]);
-            }
-        }
-    }
-    stage = b_stage;
-
-    for (int32_t tile = n_elem - 2*BK*WMMA_MKN; tile >= 0; tile-=BK*WMMA_MKN)
+    for (int32_t tile = n_elem - BK*WMMA_MKN; tile >= 0; tile-=BK*WMMA_MKN)
     {
         unsigned int ld_stage = (stage + 1)%2;
         CP_ASYNC_WAIT_GROUP(0);
         __syncthreads();
-        if(tile - BK*WMMA_MKN >= 0)
+        if(tile - BK*WMMA_MKN == n_elem -2*BK*WMMA_MKN)
+        {
+            const half* b_curr = b + (tile - BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN + gridDim.y * BN*WMMA_MKN;
+
+            for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
+                    i < BN*BK*WMMA_MKN*WMMA_MKN;
+                    i+=blockDim.x*blockDim.y*8)
+            {
+                uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
+                const half* b_gmem_curr = &b_curr[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
+                CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
+            }
+        }
+        else if(tile - BK*WMMA_MKN >= 0)
         {
             const half* a_curr = a + blockIdx.x*BM*WMMA_MKN*n_elem + tile - BK*WMMA_MKN;
             const half* b_curr = b + (tile - BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN + gridDim.y * BN*WMMA_MKN;
