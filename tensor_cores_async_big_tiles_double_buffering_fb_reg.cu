a1cd145d2b53b431f645425af556342d4742c1e5
SzymonOzog <szymon.ozog@gmail.com>
2025-06-20 19:02:37 +0200

    Forgive me father for I have hand unrolled this loop to get rid of the if condition that spilled 2 registers

--- tensor_cores_async_big_tiles_double_buffering_fb_reg.cu
+++ tensor_cores_async_big_tiles_double_buffering_fb_reg.cu
@@ -64,46 +64,29 @@ __global__ __maxnreg__(128) void tensor_core_matmul_async_swizzle_BT_DB_FB_Reg(i
     idx = idx^((idx&(S_MASK<<S_BITS_B))>>S_BITS_B);
     const uint32_t b_addr_c = __cvta_generic_to_shared(b_smem + idx);
 
-    for (int32_t tile = 0; tile < n_elem; tile+=BK*WMMA_MKN)
+    for (int32_t tile = 0; tile < n_elem-BK*WMMA_MKN; tile+=BK*WMMA_MKN)
     {
         CP_ASYNC_WAIT_GROUP(0);
         __syncthreads();
-        if(tile + BK*WMMA_MKN < n_elem)
+        unsigned int ld_stage = (stage + 1)%2;
+        const half* a_curr = a + blockIdx.x*BM*WMMA_MKN*n_elem + tile + BK*WMMA_MKN;
+        const half* b_curr = b + (tile + BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN;
+        for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
+                i < BM*BK*WMMA_MKN*WMMA_MKN;
+                i+=blockDim.x*blockDim.y*8)
         {
-            unsigned int ld_stage = (stage + 1)%2;
-            const half* a_curr = a + blockIdx.x*BM*WMMA_MKN*n_elem + tile + BK*WMMA_MKN;
-            const half* b_curr = b + (tile + BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN;
-            for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
-                    i < BM*BK*WMMA_MKN*WMMA_MKN;
-                    i+=blockDim.x*blockDim.y*8)
-            {
-                uint32_t a_smem_curr = __cvta_generic_to_shared(&a_smem[ld_stage * A_ST_STRIDE + i^((i&(S_MASK<<S_BITS_A))>>S_BITS_A)]);
-                const half* a_gmem_curr = &a_curr[(i/(WMMA_MKN*BK))*n_elem + i%(WMMA_MKN*BK)];
-                CP_ASYNC_CG(a_smem_curr, reinterpret_cast<const float4*>(a_gmem_curr), 16);
-            }
-
-            for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
-                    i < BN*BK*WMMA_MKN*WMMA_MKN;
-                    i+=blockDim.x*blockDim.y*8)
-            {
-                uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[ld_stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
-                const half* b_gmem_curr = &b_curr[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
-                CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
-            }
+            uint32_t a_smem_curr = __cvta_generic_to_shared(&a_smem[ld_stage * A_ST_STRIDE + i^((i&(S_MASK<<S_BITS_A))>>S_BITS_A)]);
+            const half* a_gmem_curr = &a_curr[(i/(WMMA_MKN*BK))*n_elem + i%(WMMA_MKN*BK)];
+            CP_ASYNC_CG(a_smem_curr, reinterpret_cast<const float4*>(a_gmem_curr), 16);
         }
-        else
-        {
-            unsigned int ld_stage = (stage + 1)%2;
-            const half* b_curr = b + (tile)*n_elem + blockIdx.y*BN*WMMA_MKN + gridDim.y * BN*WMMA_MKN;
 
-            for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
-                    i < BN*BK*WMMA_MKN*WMMA_MKN;
-                    i+=blockDim.x*blockDim.y*8)
-            {
-                uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[ld_stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
-                const half* b_gmem_curr = &b_curr[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
-                CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
-            }
+        for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
+                i < BN*BK*WMMA_MKN*WMMA_MKN;
+                i+=blockDim.x*blockDim.y*8)
+        {
+            uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[ld_stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
+            const half* b_gmem_curr = &b_curr[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
+            CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
         }
         CP_ASYNC_COMMIT_GROUP();
         uint32_t a_addr = a_addr_c + stage*A_ST_STRIDE*sizeof(half);
@@ -180,6 +163,93 @@ __global__ __maxnreg__(128) void tensor_core_matmul_async_swizzle_BT_DB_FB_Reg(i
         }
         stage = (stage+1)%2;
     }
+    unsigned int ld_stage = (stage + 1)%2;
+    const half* b_curr2 = b + (n_elem-BK*WMMA_MKN)*n_elem + blockIdx.y*BN*WMMA_MKN + gridDim.y * BN*WMMA_MKN;
+    CP_ASYNC_WAIT_GROUP(0);
+    __syncthreads();
+
+    for (int i = (threadIdx.y * blockDim.x + threadIdx.x)*8;
+            i < BN*BK*WMMA_MKN*WMMA_MKN;
+            i+=blockDim.x*blockDim.y*8)
+    {
+        uint32_t b_smem_curr = __cvta_generic_to_shared(&b_smem[ld_stage * B_ST_STRIDE + i^((i&(S_MASK<<S_BITS_B))>>S_BITS_B)]);
+        const half* b_gmem_curr = &b_curr2[(i/(BN*WMMA_MKN))*n_elem + i%(BN*WMMA_MKN)];
+        CP_ASYNC_CG(b_smem_curr, reinterpret_cast<const float4*>(b_gmem_curr), 16);
+    }
+    CP_ASYNC_COMMIT_GROUP();
+    uint32_t a_addr = a_addr_c + stage*A_ST_STRIDE*sizeof(half);
+    uint32_t b_addr = b_addr_c + stage*B_ST_STRIDE*sizeof(half);
+
+    load_tile_a_direct(a_tile[0][0], a_addr);
+    a_addr ^= 32;
+    load_tile_a_direct(a_tile[1][0], a_addr);
+
+    a_addr ^= 1056;
+    load_tile_a_direct(a_tile[0][1], a_addr);
+    a_addr ^= 32;
+    load_tile_a_direct(a_tile[1][1], a_addr);
+
+    a_addr ^= 3104;
+    load_tile_a_direct(a_tile[0][2], a_addr);
+    a_addr ^= 32;
+    load_tile_a_direct(a_tile[1][2], a_addr);
+
+    a_addr ^= 1056;
+    load_tile_a_direct(a_tile[0][3], a_addr);
+    a_addr ^= 32;
+    load_tile_a_direct(a_tile[1][3], a_addr);
+
+    load_tile_b_direct(b_tile[0], b_addr);
+    b_addr ^= 4096;
+    load_tile_b_direct(b_tile[1], b_addr);
+    b_addr ^= 4128;
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[0][m], b_tile[0], acc[m][0]);
+    }
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[1][m], b_tile[1], acc[m][0]);
+    }
+
+    load_tile_b_direct(b_tile[0], b_addr);
+    b_addr ^= 4096;
+    load_tile_b_direct(b_tile[1], b_addr);
+    b_addr ^= 4192;
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[0][m], b_tile[0], acc[m][1]);
+    }
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[1][m], b_tile[1], acc[m][1]);
+    }
+
+    load_tile_b_direct(b_tile[0], b_addr);
+    b_addr ^= 4096;
+    load_tile_b_direct(b_tile[1], b_addr);
+    b_addr ^= 4128;
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[0][m], b_tile[0], acc[m][2]);
+    }
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[1][m], b_tile[1], acc[m][2]);
+    }
+
+    load_tile_b_direct(b_tile[0], b_addr);
+    b_addr ^= 4096;
+    load_tile_b_direct(b_tile[1], b_addr);
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[0][m], b_tile[0], acc[m][3]);
+    }
+    for (int m = 0; m < OUT_TILES; m++)
+    {
+        mma(a_tile[1][m], b_tile[1], acc[m][3]);
+    }
+    stage = (stage+1)%2;
 
     for(int32_t i = 0; i<OUT_TILES; i++)
     {
@@ -189,6 +259,11 @@ __global__ __maxnreg__(128) void tensor_core_matmul_async_swizzle_BT_DB_FB_Reg(i
             int32_t output_col = matrix_b_col + j*WMMA_MKN;
             if (output_row < n_elem && output_col < n_elem)
             {
+                // for (int k = 0; k<4; k++)
+                // {
+                //     reinterpret_cast<half2*>(&c[(output_row + (lane_id>>2) + (k%2)*8)*n_elem + output_col + (k/2)*8])[lane_id%4]
+                //         = acc[i][j].x[k];
+                // }
                 int4 st;
                 int row = lane_id%16;
                 int col = (lane_id/16)*8;
@@ -379,9 +454,6 @@ double TensorCoresAsyncBT_DB_FB_RegKernel::run(half* a, half* b, half* cublas_re
     double matmul_time = std::numeric_limits<double>::max();
 
     matmul_time = std::min(matmul_time, check_configuration_async_BT_DB_FB_Reg<8, 8, 2, 4>(a, b, output, N));
-    // debug_print(b,  N, true);
-    // debug_print(output,  N, true);
-    // debug_print(cublas_ref,  N, false);
     test_output(cublas_ref, N);
 
     matmul_time = std::min(matmul_time, check_configuration_async_BT_DB_FB_Reg<16, 8, 2, 4>(a, b, output, N));
